29b6dd64b09fa9cf825826ebdb25f4fa72b089f5
Uday <uday@gmail.com>
2023-12-26 14:49:46 +0530

    Added file Main.java

---
+++ Main.java
@@ -0,0 +1,54 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void displayList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+
+        // Adding nodes to the linked list
+        linkedList.addNode(56);
+        linkedList.addNode(30);
+        linkedList.addNode(70);
+
+        // Displaying the linked list
+        System.out.println("Linked List: ");
+        linkedList.displayList();
+    }
+}
